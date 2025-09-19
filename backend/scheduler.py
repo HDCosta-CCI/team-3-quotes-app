4b9c35882df75041ee2dcb445828406df248fd50
Shivam Raikar <shivam.raikar@creativecapsule.com>
2025-09-19 12:31:07 +0530

    feat(scheduler): implement job scheduling for counting likes and dislikes on quotes

---
+++ backend/scheduler.py
@@ -0,0 +1,18 @@
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+from sqlalchemy.orm import Session
+from fastapi import Depends
+from dependencies.get_db import get_db
+from services.quote_services import QuoteServices
+
+
+def create_scheduler():
+    return AsyncIOScheduler()
+
+def add_jobs(scheduler):
+    db = next(get_db())
+    trigger = CronTrigger(second="*/10")
+    # trigger = CronTrigger(second="*/10")
+    scheduler.add_job(QuoteServices(db, user=None).count_like_dislike, trigger, id="count_job", replace_existing=True)
+
+
