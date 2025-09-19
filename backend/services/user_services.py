04e4de7d324d8fe0eb07f8257bc691b3a002b36a
HDCosta-CCI <haysten.dcosta@creativecapsule.com>
2025-09-19 16:05:42 +0530

    refactor(user_services): improve user authentication and streamline quote fetching logic

--- backend/services/user_services.py
+++ backend/services/user_services.py
@@ -11,17 +11,10 @@ class UserServices:
 
     def fetch_user_details(self):
         try:
-            if self.user is None:
-                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed!")
-            print("here", self.user)
+            self._is_authenticate_user()
             user = self.db.query(Users).filter(Users.user_id == self.user.get("user_id")).first()
 
-            data = {
-                'user_id': user.user_id,
-                'first_name': user.first_name,
-                'last_name': user.last_name,
-                'email': user.email,
-            }
+            data = self.format_data(user)
             
         except HTTPException as e:
             raise e
@@ -32,25 +25,16 @@ class UserServices:
     
     def update_user_details(self, user_update_request: UserUpdateRequest):
         try:
-            if self.user is None:
-                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed!")
-            
-            user = self.db.query(Users).filter(Users.user_id == self.user.user_id).first()
-            if not user:
-                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user not found.")
-    
+            self._is_authenticate_user()
+            user = self._check_user_exist(self.user.get("user_id"))
+
             update_user = user_update_request.model_dump(exclude_unset=True)
             self.db.query(Users).filter(Users.user_id == user.user_id).update(update_user)
 
             self.db.commit()
             self.db.refresh(user)
 
-            data = {
-                'id': user.user_id,
-                'first_name': user.first_name,
-                'last_name': user.last_name,
-                'email': user.email
-            }
+            data = self.format_data(user)
 
         except HTTPException as e:
             raise e
@@ -63,13 +47,8 @@ class UserServices:
 
     def delete_user(self, user_id):
         try:
-            if self.user is None:
-                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed!")
-            
-            user = self.db.query(Users).filter(Users.user_id == user_id).first()
-
-            if not user:
-                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user not found.")
+            self._is_authenticate_user()
+            user = self._check_user_exist(user_id)
             
             self.db.query(Users).filter(Users.user_id == user.user_id).update({Users.status : "inactive"})
             self.db.commit()
@@ -78,7 +57,6 @@ class UserServices:
 
         except HTTPException as e:
             raise e
-        
         except Exception as e:
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Internal server error: {e}")
         else:
@@ -87,26 +65,13 @@ class UserServices:
         
     def fetch_quotes(self, user_id):
         try:
-            if self.user is None:
-                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed!")
-            
+            self._is_authenticate_user()
             quotes = self.db.query(Quotes).filter(Quotes.user_id == user_id).all()
 
             if not quotes:
                 HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Authors not found.")
 
-            quotes_list = []
-
-            for q in quotes:
-                quotes_list.append({
-                    "quote": q.quote,
-                    "author": q.author,
-                    "likes": q.like,
-                    "dislikes": q.dislike,
-                    "tags": q.tags,
-                    "user_id": str(q.user_id),
-                    "quote_id": q.quote_id
-                })
+            quotes_list = self.format_quotes(quotes)
 
         except HTTPException as e:
             raise e
@@ -118,9 +83,7 @@ class UserServices:
 
     def fetch_quotes_disliked(self, user_id):
         try:
-            if self.user is None:
-                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed!")
-            
+            self._is_authenticate_user()
             quotes =  self.db.query(Quotes, UserQuoteReactions).join(UserQuoteReactions, Quotes.quote_id == UserQuoteReactions.quote_id).filter(UserQuoteReactions.user_id == user_id, UserQuoteReactions.dislike == True).all()
 
             if not quotes:        
@@ -128,16 +91,7 @@ class UserServices:
 
             quotes_list = []
             
-            for q in quotes:
-                quotes_list.append({
-                    "quote": q.quote,
-                    "author": q.author,
-                    "likes": q.like,
-                    "dislikes": q.dislike,
-                    "tags": q.tags,
-                    "user_id": str(q.user_id),
-                    "quote_id": q.quote_id
-                })
+            quotes_list = self.format_quotes(quotes)
 
         except HTTPException as e:
             raise e
@@ -150,26 +104,13 @@ class UserServices:
 
     def fetch_quotes_liked(self, user_id):
         try:
-            if self.user is None:
-                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed!")
-            
+            self._is_authenticate_user()
             quotes =  self.db.query(Quotes).join(UserQuoteReactions, Quotes.quote_id == UserQuoteReactions.quote_id).filter(UserQuoteReactions.user_id == user_id, UserQuoteReactions.like == True).all()
 
             if not quotes:        
                 HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Authors not found.")
 
-            quotes_list = []
-            
-            for q in quotes:
-                quotes_list.append({
-                    "quote": q.quote,
-                    "author": q.author,
-                    "likes": q.like,
-                    "dislikes": q.dislike,
-                    "tags": q.tags,
-                    "user_id": str(q.user_id),
-                    "quote_id": q.quote_id
-                })
+            quotes_list = self.format_quotes(quotes)
 
         except HTTPException as e:
             raise e
@@ -179,3 +120,45 @@ class UserServices:
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Internal server error: {e}")
         else:
             return quotes_list
+        
+    def _is_authenticate_user(self):
+        if not self.user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Authentication Failed!"
+            )
+    
+    def _check_user_exist(self, user_id):
+        try:
+            user = self.db.query(Users).filter(Users.user_id == user_id).first()
+            if not user:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found!")
+            return user
+        except Exception as e:
+            raise e
+        
+    def format_quotes(self, quotes):
+        quotes_list = []
+        for quote in quotes:
+            quotes_list.append({
+                "quote": quote.quote,
+                "author": quote.author,
+                "likes": quote.like,
+                "dislikes": quote.dislike,
+                "tags": quote.tags,
+                "user_id": str(quote.user_id),
+                "quote_id": quote.quote_id
+            })
+       
+        return quotes_list
+   
+    def format_data(self, user):
+        data = {
+                'id': user.user_id,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'email': user.email
+            }
+       
+        return data          
+ 
