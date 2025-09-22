901c9c0c384d1d130adf562ebf0436bda2442d1a
HDCosta-CCI <haysten.dcosta@creativecapsule.com>
2025-09-22 17:58:43 +0530

    feat: Initialize frontend with React, Vite, and routing setup
    - Added package.json for project dependencies and scripts.
    - Created main application structure with App component and routing.
    - Implemented authentication form with login functionality.
    - Developed main navigation component for routing and user actions.
    - Set up basic page components for Home, Quotes, Authors, and Error handling.
    - Integrated API utility for handling login requests.
    - Configured Vite for development and build processes.
    - Added CSS modules for styling components.

---
+++ frontend/src/layout/RootLayout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation.jsx";
+
+export default function RootLayout() {
+
+    return (
+        <>  
+            <MainNavigation />
+            <main>
+                <Outlet />
+            </main>
+        </>
+    )
+}
